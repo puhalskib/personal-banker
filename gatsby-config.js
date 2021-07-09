20438ca84fa18afde986042dbd513bb83727d5f4
ben <puhalskib@gmail.com>
2021-07-09 10:50:37 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "personal banker",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+};
