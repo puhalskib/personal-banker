baf46ba7ceba26bc2894492589d5dc61d11e7576
Ben Puhalski <puhalskib@gmail.com>
2021-07-31 19:11:55 -0400

    revert gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,11 @@
 module.exports = {
-<<<<<<< HEAD
   siteMetadata: {
-    //siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.yourdomain.tld",
     title: "personal banker",
-    description: "banking app for keeping track.",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: `blog`,
-        path: `${__dirname}/blog`,
-      },
-    },
-    "gatsby-plugin-mdx",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "posts",
-        path: `${__dirname}/content/posts/`,
-      },
-    },
   ],
-=======
-	siteMetadata: {
-		siteUrl: "https://www.yourdomain.tld",
-		title: "personal banker",
-	},
-	plugins: [
-		"gatsby-plugin-gatsby-cloud",
-		"gatsby-plugin-image",
-		"gatsby-plugin-sharp",
-	],
->>>>>>> parent of db095d3 (added graphql blog data layer)
 };
